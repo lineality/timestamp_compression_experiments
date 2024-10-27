afcb660e7bc23fa945a69bcefbecc13e37c3cd8f
lineality <lineality@gmail.com>
2024-10-27 12:15:12 -0400

    doc string

--- freshtimeproxy/src/main.rs
+++ freshtimeproxy/src/main.rs
@@ -1,7 +1,83 @@
 use chrono::{Utc, Duration, TimeZone};
 use std::collections::HashMap;
 
+/*
+Code to run and test for collisions using timestamp_freshness_proxy
+set years ahead to check.
+note: ones-place seconds are designed to collide/match
+*/
+
+
+/// ### four byte array nearly 30 year timestamp v1
+/// ## posix time scale notes
+/// ```
+/// (u1 to 1; u2 to 2; u4 to 8)
+/// 1  1 			= 1 sec
+/// 2  10			= 10 sec
+/// (u8 to 256)
+/// 3  100		= 1.67 min
+/// (u16 to 65,536; 256^2)
+/// 4  1000		= 16.7 minutes
+/// 5  10000		= 2.7 hours
+/// (u32 to 16,777,216; 256^3)
+/// 6  100000		= 1.157 days / 0.165 weeks
+/// 7  1000000 	= 0.381 months / 1.65 Weeks
+/// 8  10000000	= 3.81 months / .317 years
+/// (u64 to 4,294,967,296; 245^4)
+/// 9  100000000	= 3.17 years
+/// 10 1000000000	= 31.7 years
+/// 11 10000000000	= 317 years
+/// 12 100000000000	= 3171 years
+/// ```
+/// ## Compressed nonce-like timestamp freshness proxy
+/// Use a four u8 byte array to get a nearly 31 year nonce timestamp
+///
+/// You need 8 digits: (skip the seconds digit)
+/// ```
+/// 10 (10sec) ->  100000000 (3.17 years)
+/// +
+/// some information about the 10th digit
+/// ```
+///
+/// byte 1:
+/// - digit 2 		(in the ones place)
+/// - digit 3 		(in the tens place)
+/// - fragment-1	(in the hundreds' place), not mod !%2
+///
+/// byte 2:
+/// - digit 4 		(in the ones place)
+/// - digit 5 		(in the tens place)
+/// - fragment-2	(in the hundreds' place), not mod !%3
+///
+/// byte 3:
+/// - digit 6 		(in the ones place)
+/// - digit 7 		(in the tens place)
+/// - fragment-3	(in the hundreds' place), not 0 or 4
+///
+/// byte 4:
+/// - digit 8 		(in the ones place)
+/// - digit 9 		(in the tens place)
+/// - fragment-4	(in the hundreds' place), is prime
+///
+/// 10th digit fragments:
+/// 1. not mod !%2
+/// 2. not mod !%3
+/// 3. not 0 or 4
+/// 4. is prime
+///
+/// ## One Collision
+/// Only 5,7 values collide, but it is something.
+///
+/// ### Without Bit Manipulation
+/// This works without bitwise operations (fun though those are).
+/// There are four u8 (unsigned 8-bit) values,
+/// each of which can hold (in decimal terms)
+/// up to 0-255
+/// including 199
+///
+/// The hundres's place can safely be 1 or 0 (though it can be 2 also if we know the whole value is less than 255).
 fn generate_terse_timestamp_freshness_proxy_v4(posix_timestamp: u64) -> [u8; 4] {
+
     // 1. Extract relevant digits
     let digit_2 = ((posix_timestamp / 10) % 10) as u8;
     let digit_3 = ((posix_timestamp / 100) % 10) as u8;
